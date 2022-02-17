f8d34ffd4ed084db52d500f552c4869d20767968
Ralph Giles <rgiles@brave.com>
2022-02-17 11:48:14 -0800

    Send the ristretto base point from the example client.
    Test the `eval` wrapper in the example server by requesting
    random mixing of a sample point. This does the same thing
    as the `curl` example in the example server docs.

--- ppoprf/examples/client.rs
+++ ppoprf/examples/client.rs
@@ -15,12 +15,12 @@
 
 use env_logger::Env;
 use log::info;
-use reqwest::blocking::get;
+use reqwest::blocking::{Client, get};
 
 /// Fetch the server identification string.
 ///
 /// Acts as a basic availability ping.
-fn fetch_id(url: &str) -> reqwest::Result<()> {
+fn fetch_ident(url: &str) -> reqwest::Result<()> {
     let res = get(url)?;
     let status = res.status();
     let text = res.text()?;
@@ -30,11 +30,32 @@ fn fetch_id(url: &str) -> reqwest::Result<()> {
     Ok(())
 }
 
+/// Fetch randomness from the server.
+///
+/// Acts as a basic round-trip test.
+fn fetch_randomness(url: &str) -> reqwest::Result<()> {
+    let client = Client::new();
+    let res = client.post(url)
+        .header("Content-Type", "application/json")
+        .body("{\"name\":\"example client\", \"points\": [
+                [226, 242, 174, 10, 106, 188, 78, 113,
+                 168, 132, 169, 97, 197, 0, 81, 95,
+                 88, 227, 11, 106, 165, 130, 221, 141,
+                 182, 166, 89, 69, 224, 141, 45, 118]]}")
+        .send()?;
+    let status = res.status();
+    let text = res.text()?;
+
+    info!("{} - {}", status, text);
+
+    Ok(())
+}
 fn main() {
     let url = "http://localhost:8080";
 
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     info!("Contacting server at {}", url);
-    fetch_id(&url).unwrap();
+    fetch_ident(&url).unwrap();
+    fetch_randomness(&url).unwrap();
 }
