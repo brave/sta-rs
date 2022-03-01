72475702609e12ea177f00ddb7fbf5980386e752
Darnell Andries <dandries@brave.com>
2022-02-28 17:32:19 -0800

    Add env config to server example, support for multiple metadata tags/punctures

--- ppoprf/examples/server.rs
+++ ppoprf/examples/server.rs
@@ -28,6 +28,8 @@
 use actix_web::middleware::Logger;
 use actix_web::{get, post, web};
 use env_logger::Env;
+use dotenv::dotenv;
+use std::env;
 use log::info;
 
 use std::sync::{Arc, RwLock};
@@ -36,6 +38,11 @@ use curve25519_dalek::ristretto::CompressedRistretto;
 use ppoprf::ppoprf;
 
 use serde::{Deserialize, Serialize};
+
+const DEFAULT_EPOCH_DURATION: u64 = 5;
+const DEFAULT_MDS: &str = "116;117;118;119;120";
+const EPOCH_DURATION_ENV_KEY: &str = "EPOCH_DURATION";
+const MDS_ENV_KEY: &str = "METADATA_TAGS";
 
 #[derive(Deserialize)]
 struct EvalRequest {
@@ -49,7 +56,11 @@ struct EvalResponse {
     results: Vec<ppoprf::Evaluation>,
 }
 
-type State = Arc<RwLock<ppoprf::Server>>;
+struct ServerState {
+    prf_server: ppoprf::Server,
+    md_idx: usize
+}
+type State = Arc<RwLock<ServerState>>;
 
 #[get("/")]
 async fn index() -> &'static str {
@@ -65,12 +76,12 @@ async fn eval(
     state: web::Data<State>,
     data: web::Json<EvalRequest>,
 ) -> web::Json<EvalResponse> {
-    let server = state.read().unwrap();
+    let state = state.read().unwrap();
     // Pass each point from the client through the ppoprf.
     let result = data
         .points
         .iter()
-        .map(|p| server.eval(&p, 0, false))
+        .map(|p| state.prf_server.eval(&p, state.md_idx, false))
         .collect();
 
     // Return the results.
@@ -82,6 +93,8 @@ async fn eval(
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     let host = "localhost";
     let port = 8080;
 
@@ -89,27 +102,51 @@ async fn main() -> std::io::Result<()> {
     info!("Server configured on {} port {}", host, port);
 
     // Metadata tags marking each randomness epoch.
-    let test_mds = vec![vec!['t' as u8]];
+    let mds: Vec<Vec<u8>> = env::var(MDS_ENV_KEY)
+        .unwrap_or(DEFAULT_MDS.to_string())
+        .split(';')
+        .map(|y| {
+            y.split(',').map(|x| {
+                x.parse().expect(
+                    "Could not parse metadata tags. Must contain 8-bit unsigned values!"
+                )
+            }).collect()
+        }).collect();
+
     // Time interval between puncturing each successive md.
-    let epoch = std::time::Duration::from_secs(5);
+    let epoch = std::time::Duration::from_secs(
+        match env::var(EPOCH_DURATION_ENV_KEY) {
+            Ok(val) => {
+                val.parse().expect(
+                    "Could not parse epoch duration. It must be a positive number!"
+                )
+            },
+            Err(_) => DEFAULT_EPOCH_DURATION
+        }
+    );
 
     // Shared actix webapp state cloned into each server thread.
     // We use an RWLock to handle the infrequent puncture events.
     // Only read access is necessary to answer queries.
-    let state = Arc::new(RwLock::new(ppoprf::Server::new(&test_mds)));
+    let state = Arc::new(RwLock::new(ServerState {
+        prf_server: ppoprf::Server::new(&mds),
+        md_idx: 0
+    }));
 
     // Spawn a background task.
     let background_state = state.clone();
     actix_web::rt::spawn(async move {
         log::info!("Spawned background epoch rotation task");
         // Wait for the end of an epoch.
-        actix_web::rt::time::delay_for(epoch).await;
-        if let Ok(mut server) = background_state.write() {
-            let md = &test_mds[0];
-            log::info!("Epoch rotation: puncturing '{:?}'", md);
-            server.puncture(md);
+        for md in &mds {
+            actix_web::rt::time::delay_for(epoch).await;
+            if let Ok(mut state) = background_state.write() {
+                log::info!("Epoch rotation: puncturing '{:?}'", md);
+                state.prf_server.puncture(md);
+                state.md_idx += 1;
+            }
+            log::info!("Epoch rotation complete");
         }
-        log::info!("Epoch rotation complete");
     });
 
     // Pass a factory closure to configure the server.
