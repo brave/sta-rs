e8bcf2e249a72658e6441aa7f6b8056c92cfd2e2
Ralph Giles <rgiles@brave.com>
2022-03-09 17:09:19 -0800

    ppoprf: Blind points the example client sends.
    Demonstrate the blind/unblind steps a client would take to mix
    randomness into the hash of a real Star protocol message instead
    of always using the base point for testing.
    
    Demonstrating proof verification and finalization requires
    coordinating the epoch md with the server, probably by exposing
    an endpoint reporting the output of get_public_keys(), and a
    schedule of when different md values are used.

--- ppoprf/examples/client.rs
+++ ppoprf/examples/client.rs
@@ -13,11 +13,11 @@
 //! cargo run --example client
 //! ```
 
-use curve25519_dalek::constants::RISTRETTO_BASEPOINT_COMPRESSED;
 use env_logger::Env;
 use log::info;
+use ppoprf::ppoprf;
 use reqwest::blocking::Client as HttpClient;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 /// Fetch the server identification string.
 ///
@@ -36,25 +36,56 @@ fn fetch_ident(url: &str) -> reqwest::Result<()> {
 #[derive(Serialize)]
 struct Query {
     name: String,
-    points: Vec<String>,
+    points: Vec<ppoprf::Point>,
+}
+
+/// Explicit response body.
+#[derive(Deserialize)]
+struct Response {
+    name: String,
+    results: Vec<ppoprf::Evaluation>,
 }
 
 /// Fetch randomness from the server.
 ///
 /// Acts as a basic round-trip test.
 fn fetch_randomness(url: &str) -> reqwest::Result<()> {
+    // Construct a blinded message hash.
+    let message = "ppoprf test client";
+    let (blinded_message, r) = ppoprf::Client::blind(message.as_bytes());
+    // Submit it to the server.
     let query = Query {
         name: "example client".into(),
-        points: vec![ base64::encode(RISTRETTO_BASEPOINT_COMPRESSED.0), ],
+        points: vec![ blinded_message, ],
     };
     let res = HttpClient::new()
         .post(url)
         .json(&query)
         .send()?;
     let status = res.status();
-    let text = res.text()?;
+    let result = res.json::<Response>()?;
+    let results = result.results;
 
-    info!("{} - {}", status, text);
+    // We only submit one hash; confirm the server returned the same.
+    info!("{} {} - {} points returned", status, result.name, results.len());
+    assert_eq!(query.points.len(), results.len(),
+        "Server returned a different number of points!");
+    assert_eq!(results.len(), 1, "Expected one point!");
+    if let Some(result) = results.first() {
+        // Unblind the message hash.
+        let unblinded = ppoprf::Client::unblind(&result.output, &r);
+        // Next we would finalize the unblinded point to produce
+        // value needed for the next step of the Star protocol.
+        //
+        // This requires knowning the epoch metadata tag the server
+        // used, which we don't currently coordinate, so skip this step.
+        //let mut out = vec![0u8; ppoprf::COMPRESSED_POINT_LEN];
+        //ppoprf::Client::finalize(message.as_bytes(), &md, &unblinded, &mut out);
+        let point = base64::encode(&unblinded.as_bytes());
+        let proof = result.proof.is_some();
+        let meta = if proof { " proof" } else { "" };
+        info!("  {}{}", &point, &meta);
+    }
 
     Ok(())
 }
